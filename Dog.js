8a34572066f192bd36297adafb80d77e474c7ff7
TommyOliveir <laridatommy@gmail.com>
2022-11-03 16:07:44 +0100

    vvv

--- Dog.js
+++ Dog.js
@@ -2,31 +2,62 @@
 class Dog {
     constructor(data) {
         Object.assign(this, data)
+
+
+
     }
+
+  
     getDogHtml() {
-        const {avatar , name, age, bio, hasBeenLiked} = this
+        const { avatar, name, age, bio, } = this
+        // const show = true
+        // if (show) {
+        //     this.hasBeenLiked = true
+        // }
+
+
         // console.log(this.name)
         return `<div class="dog-individual">
 
                     <div class="dog-img">
-                        <div id="stamp" class="stamp">
-                            <img src="${ hasBeenLiked ? "/images/badge-like.png" : "/images/badge-nope.png"}" alt="w"  >
+                
+                        <div id="stamp-like" class="stamp">
+                            <img src="/images/badge-like.png"  alt="like"  >
+                        </div>
+
+                        <div id="stamp-nope" class="stamp">
+                            <img src="/images/badge-nope.png" alt="w"  >
                         </div>
                         <img src="${avatar}" alt="${name}" width="100%" >
                         <div class="dog-details">
                             <h2>${this.name}, ${age}</h2>
                             <p>${bio}</p>
                         </div>
-                       
-
+                    
                     </div>
-                    <div class="">
-                        <button>x</button>
-                        <button id="like-btn">heart</button>
-                    </div>
+                  
                
                 </div>`
+
+
     }
+    getbtnLike(){
+       
+        this.hasBeenLiked = true
+        document.getElementById("stamp-like").style.display = "block"
+        document.getElementById("stamp-nope").style.display = "none"
+       console.log(this.hasBeenLiked);
+    
+    }
+    getbtnNope(){
+       
+        this.hasBeenLiked = false
+        document.getElementById("stamp-like").style.display = "none"
+        document.getElementById("stamp-nope").style.display = "block"
+       console.log(this.hasBeenLiked);
+    
+    }
+
 }
 
 export default Dog
--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 // Remember to import the data and Dog class!
-import  Dog  from "./Dog.js"
+import Dog from "./Dog.js"
 import dogs from "./data.js"
 
+function getNewdog() {
 
+    const nextDogData = dogs.shift()
+    console.log(nextDogData)
+    return nextDogData ? new Dog(nextDogData) : {}
+
+}
 
 function render() {
-    document.getElementById("dog-list").innerHTML = rex.getDogHtml()
+    document.getElementById("dog-list").innerHTML = dogface.getDogHtml()
 }
 
 
-const rex = new Dog(dogs[0])
+
+let dogface = getNewdog()
+
 
 render()
 
-document.getElementById("like-btn").addEventListener("click", setStamp)
+document.getElementById("like-btn").addEventListener("click", stampLike)
+document.getElementById("nope-btn").addEventListener("click", stampNope)
 
- 
-function setStamp() {
-    console.log("click button")
-    document.getElementById("stamp").style.display = "block"
+function stampLike() {
+
+    if (dogs.length > 0) {
+        console.log("array length", dogs.length)
+
+        dogface.getbtnLike()
+        setTimeout(() => {
+            dogface = getNewdog()
+            document.getElementById("dog-list").innerHTML = dogface.getDogHtml()
+        }, 1000)
+    }
+    else {
+        dogface.getbtnLike()
+    }
+
 }
+
+function stampNope() {
+
+
+    if (dogs.length > 0) {
+        console.log("array length", dogs.length)
+
+        dogface.getbtnNope()
+        setTimeout(() => {
+            dogface = getNewdog()
+            document.getElementById("dog-list").innerHTML = dogface.getDogHtml()
+        }, 1500)
+    }
+    else {
+        dogface.getbtnNope()
+    }
+
+}
+
